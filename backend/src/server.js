2a56016fe1d756891b8868011a0e89a95763a487
kiethocweb0908 <kiethocweb0908@gmail.com>
2026-01-06 23:53:01 +0700

    npm install react-is

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [process.env.CLIENT_URL, "http://localhost:5173"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
